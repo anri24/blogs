bc09cc1d519817c442a4ac9647eaef773a0da032
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-05-20 12:40:47 +0400

    fix header and add logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,33 @@
 import { Link } from "react-router-dom"
 import './header.css'
+import axiosClient from "../axios-client"
+import { useStateContext } from "../contexts/ContextProvider"
 
 function Header() {
+
+    const {setUser, setToken} = useStateContext();
+
+
+    function logout(){
+        axiosClient.post('/logout')
+        .then(() => {
+            setUser({});
+            setToken(null)
+        })
+    }
+
     return (
         <header className="header">
-            <div>
-                <Link to='/' className="link main-link">Logo</Link>
-            </div>
-            <div className="header-links">
-                <Link to='/blogs' className="link">Blogs</Link>
-                <Link to='/about' className="link">About Us</Link>
-                <Link to='/contact' className="link">Contact Us</Link>
+            <div className="header-div">
+                <div>
+                    <Link to='/' className="header-link main-link">Logo</Link>
+                </div>
+                <div className="header-links">
+                    <Link to='/blogs' className="header-link">Blogs</Link>
+                    <Link to='/about' className="header-link">About Us</Link>
+                    <Link to='/contact' className="header-link">Contact Us</Link>
+                    <button onClick={logout} className="header-link">Logout</button>
+                </div>
             </div>
         </header>
     )

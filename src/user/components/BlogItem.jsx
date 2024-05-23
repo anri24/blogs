27b6643e0942fba01f6a7d6532132fe286bc1878
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-05-23 06:35:21 +0400

    getting images to users blogs page

--- src/user/components/BlogItem.jsx
+++ src/user/components/BlogItem.jsx
@@ -3,6 +3,7 @@ import './blogItem.css'
 function BlogItem({blog}) {
     return (
             <div className='blog'>
+                <img className='blog-image' src={`${import.meta.env.VITE_FILE_URL_API}/${blog.image}`} width='100%' height='150px'/>
                 <h3 className="blog-name">{blog.name}</h3>
                 <div className="blog-desc">
                     <p>{blog.description}</p>
